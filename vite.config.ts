eb1a5dd058c8e4c303a89d00a6753cd706b181b5
Tu Dinh <dinh2644.mail@gmail.com>
2023-09-14 00:24:32 -0400

    added deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import { ViteMinifyPlugin } from 'vite-plugin-minify'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),ViteMinifyPlugin({}),],
+  base: "/my-website/"
 })
